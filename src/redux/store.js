fe669b8b7123cc4c8479e757b7e9bd4b77499221
sohagraha <sohagrahabd@gmail.com>
2022-08-16 00:33:01 +0600

    feat : middleware to view previous and present

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,19 @@
 
 // import { createStore } from 'redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer);
+const myLogger = store => next => action => {
+    console.log(`Action : ${JSON.stringify(action)}`);
+    console.log(`State before : ${JSON.stringify(store.getState())}`);
+
+    const upcomingState = [action].reduce(rootReducer, store.getState());
+    console.log(`State after : ${JSON.stringify(upcomingState)}`);
+
+    next(action);
+}
+
+
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 export default store;
